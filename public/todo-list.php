b533fb3fae15f40b8227ae2a391a08e374730c0a
priest210 <ken@priest210.com>
2014-02-25 17:51:02 -0600

    added remove

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,3 +1,101 @@
+<?php
+
+
+
+// Read a file from the directory
+function read_file($filename) {
+
+    $handle = fopen($filename, 'r');
+
+    $content_string = fread($handle, filesize($filename));
+
+    $saved_array = explode("\n", $content_string);
+
+    fclose($handle);
+
+    return $saved_array;
+}
+
+// Saves an array to file name that I send
+function save_file($filename, $contents_array) {
+
+    $new_save = implode("\n", $contents_array);
+
+    $handle = fopen($filename, 'w');
+
+    fwrite($handle, $new_save);
+
+    fclose($handle);
+}
+
+// sets the filename
+$filename = 'data/todo_list.txt';
+
+
+// Read file contents
+$items = read_file($filename);
+
+// if new item is posted this code runs
+if (isset($_POST['newitem'])) {
+
+    $item = $_POST['newitem'];
+    
+    array_push($items, $item);
+
+    save_file($filename, $items);
+   
+}
+// removes item from array
+if (isset($_GET['remove'])) {
+
+    $itemId = $_GET['remove'];
+
+    unset($items[$itemId]);
+
+    save_file($filename, $items);
+   
+}
+
+//  ***
+
+
+// if (count($_FILES) > 0 && $_FILES['new_upload']['error'] == 0) {
+
+
+//     if ($FILES['new_upload']['error'] ==   {
+
+//         $errormessage = 'File upload error. ';
+//     }
+
+//     if ($FILES['new_upload']['type'] == 'text/plain') {
+
+//         $errormessage = 'File upload type error. ';
+//     }
+ 
+//     $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+    
+//     $filename = basename($_FILES['new_upload']['name']);
+    
+//     $saved_filename = $upload_dir . $filename;
+    
+//     move_uploaded_file($_FILES['new_upload']['tmp_name'], $saved_filename);
+
+//     $uploadedItems = read_file($saved_filename);
+    
+//     $items = array_merge($todo, $uploadedItems);
+
+//     save_file($filename, $items);
+
+// }
+
+// if (isset($saved_filename)) {
+
+//     echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+// }
+
+
+
+?>
 <!DOCTYPE html>
 
 <html>
@@ -11,145 +109,17 @@
      <h1>TODO List</h1>
 
         <ul>
-        	<li>Study PHP</li>
-        	<li>Study HTML</li>
-        	<li>Study PHP some more</li>
-        	<li>Master PHP</li>
+        	<? foreach ($items as $key => $value): ?>
+            <li><?= $value; ?> <a href="?remove=<?=$key;?>">Remove</a></li>
+            <? endforeach; ?>
         </ul>
 
        <h1>Add to your TODO list</h1>
 
-<?php
-
-$todo = array('Study PHP',
-              'Study HTML',
-              'Study PHP some more',
-              'Master PHP');
-
-// $todo = 
-
-// $saved_array = 
-
-foreach ($todo as $key => $item) {  
-
-        var_dump($item);
-
-}
-
-function read_file($filename) {
-
-        $handle = fopen($filename, 'r');
-
-        $contents_array = fread($handle, filesize($filename));
-
-        $saved_array = explode("\n", $contents_array);
-
-        fclose($handle);
-
-        return $saved_array;
-}
-
-function save_file($filename, $contents_array) {
-
-        $new_save = implode("\n", $contents_array);
-
-        $handle = fopen($filename, 'w');
-
-        fwrite($handle, $new_save);
-
-        fclose($handle);
-}
-
-// Using the POST method on the form in your template, 
-// create the ability to add todo items to the list. 
-
-
-
-// Each time an item is added, the todo list file should be saved 
-// with the new item added.
-
-
-
-// need to take uploaded file from form and put in array
-
-
-// if (isset($_POST['newitem']) && !empty {  )
-
-    # code...
-// }
-
-// Add a link next to each todo item that says "Mark Complete" and 
-
-// have it send a GET request to the page that deletes the entry. 
-
-// Use query strings to send the proper key back to the server, and 
-// update the todo list file to reflect the deletion.
-
-
-if (count($_FILES) > 0 && $_FILES['new_upload']['error'] == 0) {
-
-
-    // if ($FILES['new_upload']['error'] ==   {
-
-    //     $errormessage = 'File upload error. ';
-    // }
-
-    // if ($FILES['new_upload']['type'] == 'text/plain') {
-
-    //     $errormessage = 'File upload type error. ';
-    // }
- 
-    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
-    
-    $filename = basename($_FILES['new_upload']['name']);
-    
-    $saved_filename = $upload_dir . $filename;
-    
-    move_uploaded_file($_FILES['new_upload']['tmp_name'], $saved_filename);
-
-    // $uploadedItems = read_file($saved_filename);
-    
-    // $todos = array_merge($todo, $uploadedItems);
-
-    // save_file($filename, $items);
-
-}
-
-if (isset($saved_filename)) {
-
-    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
-}
-
-if (isset($_POST['newitem'])) {
-
-    $item = $_POST['newitem'];
-    
-    array_push($items, $item);
-
-    save_file($filename, $items);
-   
-}
-
-if (isset($_GET ['remove'])) {
-
-    $itemId = $_GET['remove'];
-
-    unset($items[$itmeId]);
-
-    save_file($filename, $items);
-   
-}
-
-
-
-
-?>
-
-
 <form method="POST" action="">
     <p>
-        <label for="New Item">Add item:</label>
-        <input id="New Item" name="New Item" type="text" autotfocus = "autofocus" placeholder="Enter New TODO Item">
+        <label for="newitem">Add item:</label>
+        <input id="newitem" name="newitem" type="text" autotfocus = "autofocus" placeholder="Enter New TODO Item">
     </p>
 
     <p>
@@ -175,17 +145,17 @@ if (isset($_GET ['remove'])) {
 </form>
 
 <?php 
-echo "GET:";
+// echo "GET:";
 
-var_dump($_GET);
+// var_dump($_GET);
 
-echo "POST";
+// echo "POST";
 
-var_dump($_GET);
+// var_dump($_GET);
 
-echo "FILES";
+// echo "FILES";
 
-var_dump($_FILES);
+// var_dump($_FILES);
 
 
  ?>
