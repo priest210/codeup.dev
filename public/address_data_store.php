6efaf0ae1f9965f8cf2168508103acb05b39bd32
priest210 <ken@priest210.com>
2014-02-28 18:07:24 -0600

    working on inheritance

---
+++ public/address_data_store.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('filestore.php');
+
+class AddressDataStore extends Filestore {
+
+
+}
+
+// class Contacts {
+
+// 	public $name = '';
+// 	public $streetaddress = '';
+// 	public $city = '';
+// 	public $state = '';
+// 	public $zip = '';
+
+
+// }
