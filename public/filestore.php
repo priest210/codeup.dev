3ccbceeb3b978ffb94ad54855d2666e98bdc09e5
priest210 <ken@priest210.com>
2014-03-03 18:32:14 -0600

    just in case after refactoring for today's exercises

--- public/filestore.php
+++ public/filestore.php
@@ -4,13 +4,36 @@ class Filestore {
 
     public $filename = '';
 
-    function __construct($filename = '') {
-        if(!empty($filename)) {
-        	$this->filename = $filename;
+    private $is_csv = FALSE;
+
+// Sets filename
+    public function __construct($filename = '') {
+        $this->filename = $filename;
+        if (substr($filename, -3) == 'csv') {
+        	$this->is_csv = TRUE;
+        } 
+
+    }
+// Decides if file is a CSV or string and will read it accordingly
+    public function read() {
+        if ($this->is_csv) {
+            return $this->read_csv();
+        }   else {
+            return $this->read_lines();
         }
     }
 
-    function read_lines()
+    public function write($array) {
+        if ($this->is_csv) {
+            $this->write_csv($array);
+        } else {
+            $this->write_lines($array);
+        }
+
+    }
+
+// Reads contents of string and push to array
+    private function read_lines()
     {
     	$filesize = filesize($this->filename);
     	
@@ -26,18 +49,17 @@ class Filestore {
     	return $saved_array;
 
     }
-
-    /**
-     * Writes each element in $array to a new line in $this->filename
-     */
-    function write_lines($contents_array) {
+// Writes contents of array to string
+    private function write_lines($contents_array) {
     	$new_save = implode("\n", $contents_array);
     	$handle = fopen($this->filename, 'w');
     	fwrite($handle, $new_save);
     	fclose($handle);
 	}
 
-    public function read_address_book() {
+
+// Reads CSV and returns array
+    private function read_csv() {
     	$filesize = filesize($this->filename);
         $handle = fopen($this->filename, 'r');
 
@@ -55,8 +77,8 @@ class Filestore {
     	return $contacts;
 
 	}
-
-    public function write_address_book($contacts) {
+// Writes contents of array to CSV file.
+    private function write_csv($contacts) {
     	$handle = fopen($this->filename, 'w');
     	foreach ($contacts as $row) {
     		fputcsv($handle, $row);
